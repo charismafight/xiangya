0b234af07bca1bf96bbb557cc7d41029e6fd66a6
Lee-Comp <charismafight@hotmail.com>
2024-12-31 01:13:43 +0800

    feat: contact component

--- xiangya/src/components/Contact.vue
+++ xiangya/src/components/Contact.vue
@@ -1,19 +1,39 @@
 <template>
-    <Information title="直播地址">
-        <div style="text-align: center;">
-            <img src="../assets/images/live_qrcode.png" height="240" width="240">
-        </div>
-        <div style="text-align: center;">
-            <el-button type="primary" plain @click="redirect">直接跳转到直播间</el-button>
-        </div>
-    </Information>
+  <Information title="联系我们">
+    <el-row class="main" :gutter="0">
+      <p>
+        大会秘书：<br />胡永斌
+        <el-link type="primary" href="tel:15073153766">15073153766</el-link>，傅晓丹
+        <el-link type="primary" href="tel:13739090971">13739090971</el-link><br /><br />
+        病理技术分会场：<br />朱薇
+        <el-link type="primary" href="tel:13467502963">13467502963</el-link>，谢斌<el-link
+          type="primary"
+          href="tel:15074808393"
+          >15074808393</el-link
+        ><br /><br />
+        肿瘤新进展湘雅论坛分会场：<br />石颖
+        <el-link type="primary" href="tel:18974914446">18974914446</el-link><br /><br />
+        病理取材规范化培训班：<br />杨可达
+        <el-link type="primary" href="tel:13170417861">13170417861</el-link><br /><br />
+        疑难病例讨论分享：<br />胡永斌
+        <el-link type="primary" href="tel:15073153766">15073153766</el-link><br /><br />
+        病理技术石蜡切片比赛：<br />朱薇
+        <el-link type="primary" href="tel:13467502963">13467502963</el-link>，杨博
+        <el-link type="primary" href="tel:15974339100">15974339100</el-link><br /><br />
+        病理医师识图比赛：<br />王颖，罗柏花
+        <el-link type="primary" href="tel:13874869576">13874869576</el-link>
+      </p>
+    </el-row>
+  </Information>
 </template>
 
-<script setup lang="ts">
-import Information from './Information.vue';
-
-const redirect = () => {
-    window.open('https://wx.vzan.com/live/page/903169705?v=1727332090300&ver=5cf35c9984144221867852ba24f6affa', '_blank');
-}
-
+<script setup>
+import Information from './Information.vue'
 </script>
+
+<style scoped>
+p {
+  width: 100%;
+  line-height: 30px;
+}
+</style>
